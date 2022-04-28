4bdedc83c222fba70d0fe543653a6fba1d0d05db
Adolfovi <dolfillo99@gmail.com>
2022-04-28 12:40:59 +0200

    Modificación js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
+// buton menu
 let hamburger = document.getElementById('hamburger-menu-button');
-let hamburgerSvg = document.getElementById('hamburger-menu-button-svg');
 let navMenu = document.getElementById('nav-menu-mobile');
+let hamburgerSvg = document.getElementById('hamburger-menu-button-svg');
 
 
 
 hamburger.addEventListener('click', () => {
 
-navMenu.classList.toggle('active');
-hamburgerSvg.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    hamburgerSvg.classList.toggle('active');
 
+});
+
+// botón más menos
+let minus = document.querySelector('.quantity-minus');
+let plus = document.querySelector('.quantity-plus');
+let quantity = document.querySelector('#input-quantity');
+
+plus.addEventListener('click', () => {
+    parseInt(quantity.value++);
+});
+
+minus.addEventListener('click', () => {
+    parseInt(quantity.value--);
+    if (quantity.value < 1) {
+        quantity.value = 1;
+    }
 });
