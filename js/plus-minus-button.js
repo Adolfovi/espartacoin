dda37578558627afd4e85a431a1c3dfec04489e9
Adolfovi <dolfillo99@gmail.com>
2022-05-03 13:18:35 +0200

    Inserción de nuevas mejoras

--- js/plus-minus-button.js
+++ js/plus-minus-button.js
@@ -2,43 +2,25 @@ export let renderPlusMinusButton = () => {
 
     let minuses = document.querySelectorAll('.quantity-minus');
     let pluses = document.querySelectorAll('.quantity-plus');
-    let input = document.querySelector('.input-quantity');
+    
     pluses.forEach(plus => {
 
         plus.addEventListener('click', () => {
-            let fatherofplus = plus.parentNode;
-            let firstchildoffatherofplus = fatherofplus.firstElementChild;
-            let nextsiblingoffirstchildoffatherofplus = firstchildoffatherofplus.nextElementSibling;
-            let firstchildofnextsiblingoffirstchildoffatherofplus = nextsiblingoffirstchildoffatherofplus.firstElementChild;
-            let firstchildoffirstchildofnextsiblingoffirstchildoffatherofplus = firstchildofnextsiblingoffirstchildoffatherofplus.firstElementChild;
-            console.log(fatherofplus);
-            parseInt(firstchildoffirstchildofnextsiblingoffirstchildoffatherofplus.value++);
+
+            let plusMinusInput = plus.parentNode.querySelector('.input-quantity');
+           
+            parseInt(plusMinusInput.value++);
         });
     });
 
     minuses.forEach(minus => {
         minus.addEventListener('click', () => {
-            let fatherofminus = minus.parentNode;
-            let firstchildoffatherofminus = fatherofminus.firstElementChild;
-            let nextsiblingoffirstchildoffatherofminus = firstchildoffatherofminus.nextElementSibling;
-            let firstchildofnextsiblingoffirstchildoffatherofminus = nextsiblingoffirstchildoffatherofminus.firstElementChild;
-            let firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus = firstchildofnextsiblingoffirstchildoffatherofminus.firstElementChild;
 
-            console.log(fatherofminus);
+            let plusMinusInput =  minus.parentNode.querySelector('.input-quantity');;
 
-            if (firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus.value > 1) {
-                parseInt(firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus.value--);
+            if (plusMinusInput.value > 1) {
+                parseInt(plusMinusInput.value--);
             }
-
-
         });
     });
-
-
-
-
-
-
-
-
 }
