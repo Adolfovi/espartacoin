4ff75f2dd914c533e153cdf74ced9d8c703e86eb
Adolfovi <dolfillo99@gmail.com>
2022-05-03 11:06:12 +0200

    Reparación de los botones de más y menos

--- js/plus-minus-button.js
+++ js/plus-minus-button.js
@@ -2,26 +2,43 @@ export let renderPlusMinusButton = () => {
 
     let minuses = document.querySelectorAll('.quantity-minus');
     let pluses = document.querySelectorAll('.quantity-plus');
-    let quantities = document.querySelectorAll('.input-quantity');
-
+    let input = document.querySelector('.input-quantity');
     pluses.forEach(plus => {
 
         plus.addEventListener('click', () => {
-            quantities.forEach(quantity => {
-                parseInt(quantity.value++);
-            });
+            let fatherofplus = plus.parentNode;
+            let firstchildoffatherofplus = fatherofplus.firstElementChild;
+            let nextsiblingoffirstchildoffatherofplus = firstchildoffatherofplus.nextElementSibling;
+            let firstchildofnextsiblingoffirstchildoffatherofplus = nextsiblingoffirstchildoffatherofplus.firstElementChild;
+            let firstchildoffirstchildofnextsiblingoffirstchildoffatherofplus = firstchildofnextsiblingoffirstchildoffatherofplus.firstElementChild;
+            console.log(fatherofplus);
+            parseInt(firstchildoffirstchildofnextsiblingoffirstchildoffatherofplus.value++);
         });
     });
 
     minuses.forEach(minus => {
         minus.addEventListener('click', () => {
-            quantities.forEach(quantity => {
-                if (quantity.value > 1) {
-                    parseInt(quantity.value--);
-                }
-            });
+            let fatherofminus = minus.parentNode;
+            let firstchildoffatherofminus = fatherofminus.firstElementChild;
+            let nextsiblingoffirstchildoffatherofminus = firstchildoffatherofminus.nextElementSibling;
+            let firstchildofnextsiblingoffirstchildoffatherofminus = nextsiblingoffirstchildoffatherofminus.firstElementChild;
+            let firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus = firstchildofnextsiblingoffirstchildoffatherofminus.firstElementChild;
+
+            console.log(fatherofminus);
+
+            if (firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus.value > 1) {
+                parseInt(firstchildoffirstchildofnextsiblingoffirstchildoffatherofminus.value--);
+            }
+
 
         });
     });
+
+
+
+
+
+
+
 
 }
