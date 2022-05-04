c514f7563d56c2f1ab0ebbaca22ff1d6aa4c96ff
Adolfovi <dolfillo99@gmail.com>
2022-05-04 11:07:59 +0200

    Inserción de javascript extra

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 import { renderTabs } from './tabs.js';
 import { renderPlusMinusButton } from './plus-minus-button.js';
 import { renderMenuButton } from './menu-button.js';
+import { buttoncheckout } from './button-checkout.js';
+
 
 renderTabs();
 renderPlusMinusButton();
 renderMenuButton();
+buttoncheckout();
