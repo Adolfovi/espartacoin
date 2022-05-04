bb2418fb0e9e6927b719b2c33f3b95bdbe4d9383
Adolfovi <dolfillo99@gmail.com>
2022-05-04 13:44:04 +0200

    Modificación del sistema de carrito

--- js/notification.js
+++ js/notification.js
@@ -12,9 +12,15 @@ export let notification = (message, type) => {
         if (type == "success") {
             notification.classList.remove('active');
             notification.classList.add('active-hidden');
+            setTimeout(() => {
+                notification.classList.remove('active-hidden');
+            }, 1000);
         } else {
             notification.classList.remove('active-error');
             notification.classList.add('active-error-hidden');
+            setTimeout(() => {
+                notification.classList.remove('active-error-hidden');
+            }, 1000);
         }
     }, 4000);
 }
