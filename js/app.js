78a80d944873ef3def26999fe11f1b534733c774
Adolfovi <dolfillo99@gmail.com>
2022-05-11 10:28:52 +0200

    Creación de faqs

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ import { renderMenuButton } from './menu-button.js';
 import { buyproduct } from './buy-product.js';
 import { renderNotification } from './notification.js';
 import { renderformcheckout } from './form-checkout.js';
-
+import { renderbuttonfaq } from './button-faq.js';
 renderTabs();
 renderPlusMinusButton();
 renderMenuButton();
 buyproduct();
 renderNotification();
 renderformcheckout();
+renderbuttonfaq();
