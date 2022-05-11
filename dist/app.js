ee937d429323de45dc61a3831e59661e4912a943
Adolfovi <dolfillo99@gmail.com>
2022-05-11 12:10:19 +0200

    Creación de webpack

---
+++ dist/app.js
@@ -0,0 +1,156 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/app.js":
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _tabs_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tabs.js */ \"./js/tabs.js\");\n/* harmony import */ var _plus_minus_button_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./plus-minus-button.js */ \"./js/plus-minus-button.js\");\n/* harmony import */ var _menu_button_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu-button.js */ \"./js/menu-button.js\");\n/* harmony import */ var _buy_product_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./buy-product.js */ \"./js/buy-product.js\");\n/* harmony import */ var _notification_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./notification.js */ \"./js/notification.js\");\n/* harmony import */ var _form_checkout_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./form-checkout.js */ \"./js/form-checkout.js\");\n/* harmony import */ var _button_faq_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./button-faq.js */ \"./js/button-faq.js\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n(0,_tabs_js__WEBPACK_IMPORTED_MODULE_0__.renderTabs)();\r\n(0,_plus_minus_button_js__WEBPACK_IMPORTED_MODULE_1__.renderPlusMinusButton)();\r\n(0,_menu_button_js__WEBPACK_IMPORTED_MODULE_2__.renderMenuButton)();\r\n(0,_buy_product_js__WEBPACK_IMPORTED_MODULE_3__.buyproduct)();\r\n(0,_notification_js__WEBPACK_IMPORTED_MODULE_4__.renderNotification)();\r\n(0,_form_checkout_js__WEBPACK_IMPORTED_MODULE_5__.renderformcheckout)();\r\n(0,_button_faq_js__WEBPACK_IMPORTED_MODULE_6__.renderbuttonfaq)();\n\n//# sourceURL=webpack:///./js/app.js?");
+
+/***/ }),
+
+/***/ "./js/button-faq.js":
+/*!**************************!*\
+  !*** ./js/button-faq.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderbuttonfaq\": () => (/* binding */ renderbuttonfaq)\n/* harmony export */ });\nlet renderbuttonfaq = () => {\r\n    let buttonsfaq = document.querySelectorAll('.faq-arrow');\r\n    buttonsfaq.forEach(buttonfaq => {\r\n        buttonfaq.addEventListener('click', () => {\r\n            buttonfaq.classList.toggle('active');\r\n            buttonfaq.parentElement.parentElement.querySelector(\".faq-submain\").classList.toggle('active');\r\n        })\r\n    });\r\n}\n\n//# sourceURL=webpack:///./js/button-faq.js?");
+
+/***/ }),
+
+/***/ "./js/buy-product.js":
+/*!***************************!*\
+  !*** ./js/buy-product.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"buyproduct\": () => (/* binding */ buyproduct)\n/* harmony export */ });\nlet buyproduct = () => {\r\n    let buyproduct = document.querySelector('.buy-product');\r\n\r\n    if (buyproduct) {\r\n\r\n        buyproduct.addEventListener('click', (event) => {\r\n\r\n            event.preventDefault();\r\n\r\n            document.dispatchEvent(new CustomEvent('message', {\r\n                detail: {\r\n                    text: 'Producto añadido a la cesta',\r\n                    type: 'success'\r\n                }\r\n            }));\r\n        });\r\n\r\n    }\r\n\r\n}\n\n//# sourceURL=webpack:///./js/buy-product.js?");
+
+/***/ }),
+
+/***/ "./js/form-checkout.js":
+/*!*****************************!*\
+  !*** ./js/form-checkout.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderformcheckout\": () => (/* binding */ renderformcheckout)\n/* harmony export */ });\nlet renderformcheckout = () => {\r\n\r\n    let buttoncheckout = document.querySelector('.pay-continue');\r\n    if (buttoncheckout) {\r\n        buttoncheckout.addEventListener('click', (event) => {\r\n\r\n            event.preventDefault();\r\n\r\n            document.dispatchEvent(new CustomEvent('message', {\r\n                detail: {\r\n                    text: 'Formulario Enviado',\r\n                    type: 'success'\r\n                }\r\n            }));\r\n\r\n        });\r\n\r\n    }\r\n\r\n}\n\n//# sourceURL=webpack:///./js/form-checkout.js?");
+
+/***/ }),
+
+/***/ "./js/menu-button.js":
+/*!***************************!*\
+  !*** ./js/menu-button.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderMenuButton\": () => (/* binding */ renderMenuButton)\n/* harmony export */ });\nlet renderMenuButton = () => {\r\n\r\n    let hamburger = document.getElementById('hamburger-menu-button');\r\n    let navMenu = document.getElementById('nav-menu-mobile');\r\n    let hamburgerSvg = document.getElementById('hamburger-menu-button-svg');\r\n\r\n    if(hamburger) {\r\n        hamburger.addEventListener('click', () => {\r\n\r\n            navMenu.classList.toggle('active');\r\n            hamburger.classList.toggle('active');\r\n            hamburgerSvg.classList.toggle('active');\r\n\r\n        });\r\n    }\r\n}\n\n//# sourceURL=webpack:///./js/menu-button.js?");
+
+/***/ }),
+
+/***/ "./js/notification.js":
+/*!****************************!*\
+  !*** ./js/notification.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderNotification\": () => (/* binding */ renderNotification)\n/* harmony export */ });\nlet renderNotification = () => {\r\n\r\n    document.addEventListener('message', (event) => {\r\n\r\n        let notification = document.querySelector('.notification');\r\n        let notificationMessage = document.querySelector('.notification-text');\r\n        \r\n            notificationMessage.innerHTML = event.detail.text;\r\n            notification.classList.add(event.detail.type);\r\n            notification.classList.add('active');\r\n\r\n            setTimeout(() => {\r\n                notification.classList.remove('active');\r\n                notification.classList.remove(event.detail.type);\r\n            }, 4000);\r\n\r\n\r\n        \r\n\r\n    });\r\n\r\n\r\n}\n\n//# sourceURL=webpack:///./js/notification.js?");
+
+/***/ }),
+
+/***/ "./js/plus-minus-button.js":
+/*!*********************************!*\
+  !*** ./js/plus-minus-button.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderPlusMinusButton\": () => (/* binding */ renderPlusMinusButton)\n/* harmony export */ });\nlet renderPlusMinusButton = () => {\r\n\r\n    let minuses = document.querySelectorAll('.quantity-minus');\r\n    let pluses = document.querySelectorAll('.quantity-plus');\r\n    \r\n    pluses.forEach(plus => {\r\n\r\n        plus.addEventListener('click', () => {\r\n\r\n            let plusMinusInput = plus.parentNode.querySelector('.input-quantity');\r\n           \r\n            parseInt(plusMinusInput.value++);\r\n        });\r\n    });\r\n\r\n    minuses.forEach(minus => {\r\n        minus.addEventListener('click', () => {\r\n\r\n            let plusMinusInput =  minus.parentNode.querySelector('.input-quantity');;\r\n\r\n            if (plusMinusInput.value > 1) {\r\n                parseInt(plusMinusInput.value--);\r\n            }\r\n        });\r\n    });\r\n}\n\n//# sourceURL=webpack:///./js/plus-minus-button.js?");
+
+/***/ }),
+
+/***/ "./js/tabs.js":
+/*!********************!*\
+  !*** ./js/tabs.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderTabs\": () => (/* binding */ renderTabs)\n/* harmony export */ });\nlet renderTabs = () => {\r\n    let select = document.querySelector('.select-type');\r\n    let relateds = document.querySelectorAll('.type-related');\r\n\r\n    if (select) {\r\n        select.addEventListener('change', () => {\r\n            relateds.forEach(related => {\r\n                related.classList.remove('active');\r\n                if (select.value == related.dataset.elementtype) {\r\n                    related.classList.add('active');\r\n                }\r\n            });\r\n        });\r\n    }\r\n}\n\n//# sourceURL=webpack:///./js/tabs.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/app.js");
+/******/ 	
+/******/ })()
+;
--- js/notification.js
+++ js/notification.js
@@ -4,16 +4,20 @@ export let renderNotification = () => {
 
         let notification = document.querySelector('.notification');
         let notificationMessage = document.querySelector('.notification-text');
+        
+            notificationMessage.innerHTML = event.detail.text;
+            notification.classList.add(event.detail.type);
+            notification.classList.add('active');
 
-        notificationMessage.innerHTML = event.detail.text;
-        notification.classList.add(event.detail.type);
-        notification.classList.add('active');
-    
-        setTimeout(() => {
-            notification.classList.remove('active');
-            notification.classList.remove(event.detail.type);
-        }, 4000);
+            setTimeout(() => {
+                notification.classList.remove('active');
+                notification.classList.remove(event.detail.type);
+            }, 4000);
+
+
+        
+
     });
 
-   
+
 }
